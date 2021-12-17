954871aa56e8c57cb66daf2df09481e83244e3e2
martinleonardo <79655801+martinleonardo@users.noreply.github.com>
2021-12-17 10:20:28 -0800

    add files to run bot

---
+++ main.py
@@ -0,0 +1,38 @@
+import discord
+import os
+from discord.ext import commands
+
+client = commands.Bot(command_prefix='.')
+
+
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+    print('Loaded ', extension)
+
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    print('Unloaded ', extension)
+
+
+@client.command(description="[all] - reload cogs")
+async def reload(ctx, extension):
+    if extension == 'all':
+        for filename in os.listdir('./cogs'):
+            if filename.endswith('.py'):
+                client.unload_extension(f'cogs.{filename[:-3]}')
+                client.load_extension(f'cogs.{filename[:-3]}')
+        print('All extensions reloaded')
+    else:
+        client.unload_extension(f'cogs.{extension}')
+        client.load_extension(f'cogs.{extension}')
+        print('Reloaded ', extension)
+
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
+client.run(os.environ.get('DISCORD_BOT_TOKEN'))
