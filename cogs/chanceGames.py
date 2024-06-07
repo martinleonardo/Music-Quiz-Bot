078fb9e59504237967b625c6cb18e855787f6f66
martinleonardo <martinccleonardo@gmail.com>
2024-06-07 08:36:48 -0700

    Automate deathroll rounds

--- cogs/chanceGames.py
+++ cogs/chanceGames.py
@@ -6,13 +6,19 @@ import random
 import time
 
 class PlayerData:
-    def __init__(self, upper_limit):
-        self.upper_limit = upper_limit
+    def __init__(self, player_name, upper_limit):
+        self.old_limit = upper_limit
+        self.new_limit = upper_limit
         self.last_update = time.time()
+        self.player_name = player_name
+        self.roll_round = 0
+        self.lost = False
     
 
 class ChanceGames(commands.Cog):
     expiration_time = 1800      # Time in seconds
+    max_roll = 100
+    lol_threshold = 10          # Inclusive
 
     def __init__(self, client):
         self.client = client
@@ -23,6 +29,56 @@ class ChanceGames(commands.Cog):
     
     def expired_roll(self, player_id):
         return self.deathroll_player_rolls[player_id].last_update + self.expiration_time < time.time()
+    
+    def game_in_progress(self):
+        # Can join if all active players are at round 1
+        for player_id in self.deathroll_player_rolls.keys():
+            if self.deathroll_player_rolls[player_id].roll_round > 1:
+                return True
+        return False
+
+    def delete_old_players(self):
+        for player_id in self.deathroll_player_rolls.keys():
+            if self.expired_roll(player_id):
+                del self.deathroll_player_rolls[player_id]
+        return
+
+    def unrolled_players(self, roll_round):
+        players = []
+        self.delete_old_players()
+        for player_id in self.deathroll_player_rolls.keys():
+            if roll_round > self.deathroll_player_rolls[player_id].roll_round and not self.deathroll_player_rolls[player_id].lost:
+                players.append(self.deathroll_player_rolls[player_id].player_name)
+        return players
+    
+    def all_rolled_1(self):
+        all_rolls_1 = True
+        for player_id in self.deathroll_player_rolls.keys():
+            if self.deathroll_player_rolls[player_id].new_limit > 1:
+                all_rolls_1 = False
+        return all_rolls_1
+
+    def mark_lost_players(self):
+        for player_id in self.deathroll_player_rolls.keys():
+            if self.deathroll_player_rolls[player_id].new_limit == 1:
+                self.deathroll_player_rolls[player_id].lost = True
+        return
+    
+    def revert_limit(self):
+        for player_id in self.deathroll_player_rolls.keys():
+            if not self.deathroll_player_rolls[player_id].lost:
+                self.deathroll_player_rolls[player_id].new_limit = self.deathroll_player_rolls[player_id].old_limit
+        return
+    
+    def only_winner_left(self):
+        winner = None
+        for player_id in self.deathroll_player_rolls.keys():
+            if not self.deathroll_player_rolls[player_id].lost:
+                if winner:
+                    # Multiple winners
+                    return None
+                winner = self.deathroll_player_rolls[player_id].player_name
+        return winner
 
     @commands.command(description="flip a coin", aliases=['coin_flip', 'flip', 'coin'])
     async def flip_coin(self, ctx):
@@ -51,16 +107,22 @@ class ChanceGames(commands.Cog):
         except:
             await ctx.send('Please provide a valid integer')
 
+    # TODO Add turn order?
+    # TODO Add server info so users can play across different servers
     @commands.command(description="Death roll. Option to add 'reset' or 'resetall',", aliases=['dr', 'deathroll', 'droll'])
-    async def death_roll(self, ctx, arg='none'):
+    async def death_roll(self, ctx, *args):
         player_id = ctx.message.author.id
+        player_name = ctx.message.author.name
 
-        arg = arg.lower()
+        if not args:
+            args = ["roll"]
+
+        arg = args[0].lower()
         match(arg):
-            case "none":
+            case "roll":
                 pass
             case "reset":
-                self.deathroll_player_rolls[player_id] = PlayerData(100)
+                self.deathroll_player_rolls[player_id] = PlayerData(player_name, self.max_roll)
                 await ctx.send('Resetting roll for player ' + ctx.message.author.name)
                 return
             case "resetall":
@@ -68,32 +130,71 @@ class ChanceGames(commands.Cog):
                 await ctx.send('Resetting all player rolls')
                 return
             case "list":
-                for k in self.deathroll_player_rolls.keys():
-                    await ctx.send(str(k) + " has " + str(self.deathroll_player_rolls[k].upper_limit))
+                for temp_player_id in self.deathroll_player_rolls.keys():
+                    await ctx.send(self.deathroll_player_rolls[temp_player_id].player_name + " has a max roll of " + str(self.deathroll_player_rolls[temp_player_id].upper_limit) + " after " + self.deathroll_player_rolls[temp_player_id].roll_round + " rolls")
+                return
+            case "set":
+                if not args[1] or not args[1].isnumeric():
+                    await ctx.send('Please provide a valid value to set upper limit to. Using old limit for now.')
+                    self.deathroll_player_rolls[player_id].new_limit = self.deathroll_player_rolls[player_id].old_limit
+                    return
+                
+                limit = args[1]
+                self.deathroll_player_rolls[player_id].old_limit = self.deathroll_player_rolls[player_id].new_limit = min(self.max_roll, int(limit))
                 return
             case _:
-                await ctx.send('Invalid argument ' + arg + '. Ignoring.')
+                if arg.isnumeric():
+                    self.deathroll_player_rolls[player_id].old_limit = self.deathroll_player_rolls[player_id].new_limit = min(self.max_roll, int(arg))
+                    await ctx.send('Resetting max roll to ' + str(min(self.max_roll, int(arg))))
+                    return
+                await ctx.send('Invalid argument ' + arg + '. Rolling as usual instead.')
         
-
-        if self.new_player(player_id):
-            self.deathroll_player_rolls[player_id] = PlayerData(100)
-            await ctx.send('New player. Starting roll.')
-        
-        elif self.expired_roll(player_id):
-            self.deathroll_player_rolls[player_id] = PlayerData(100)
-            await ctx.send('New player. Starting roll.')
-
-        old_limit = self.deathroll_player_rolls[player_id].upper_limit
-        new_limit = random.randint(1, old_limit)
-        
-        await ctx.send('Your number is **' + str(new_limit) + '** out of ' + str(old_limit) + '.')
-
-        if new_limit != 1:
-            self.deathroll_player_rolls[player_id].upper_limit = new_limit
+        if self.new_player(player_id) or self.expired_roll(player_id):
+            if self.game_in_progress():
+                await ctx.send('Player cannot join due to game being in progress. Please wait for round to end.')
+                return
+            else:
+                self.deathroll_player_rolls[player_id] = PlayerData(player_name, self.max_roll)
+                await ctx.send('New player. Starting roll.')
         else:
-            await ctx.send('You have hit **1**. Keeping old roll for ties. Reset (!dr reset) to start over.')
-        
+            players_remaining = self.unrolled_players(self.deathroll_player_rolls[player_id].roll_round)
+            if players_remaining:
+                await ctx.send('Cannot roll. Waiting on players: ' + str(players_remaining))
+                return
 
+        self.deathroll_player_rolls[player_id].old_limit = self.deathroll_player_rolls[player_id].new_limit
+        self.deathroll_player_rolls[player_id].new_limit = random.randint(1, self.deathroll_player_rolls[player_id].old_limit)
+        self.deathroll_player_rolls[player_id].roll_round += 1
+        self.deathroll_player_rolls[player_id].last_update = time.time()
+        
+        await ctx.send('Your number is **' + str(self.deathroll_player_rolls[player_id].new_limit) + '** out of ' + str(self.deathroll_player_rolls[player_id].old_limit) + ' for round ' + str(self.deathroll_player_rolls[player_id].roll_round))
+
+        players_remaining = self.unrolled_players(self.deathroll_player_rolls[player_id].roll_round)
+        if players_remaining:
+            # Waiting for other players to finish rolls for round
+            return
+
+        if self.deathroll_player_rolls[player_id].new_limit == 1:
+            # Should handle single player differently
+            if self.deathroll_player_rolls[player_id].roll_round == 1 and self.max_roll >= self.lol_threshold:
+                await ctx.send('lol')
+            if self.all_rolled_1():
+                await ctx.send('All remaining players rolled 1. Redoing round.')
+                self.revert_limit()
+                return
+            
+        # Ignore first round since others will probably be joining
+        if self.deathroll_player_rolls[player_id].roll_round > 1 and len(self.deathroll_player_rolls) > 1:
+            self.mark_lost_players()
+            winner = self.only_winner_left()
+            if winner:
+                await ctx.send("The winner is **" + winner + "** after " + str(self.deathroll_player_rolls[player_id].roll_round) + " rounds!")
+
+                await ctx.send("Resetting game")
+                self.deathroll_player_rolls = {}
+                return
+            await ctx.send('All players have rolled for the round. You may begin round ' + str(self.deathroll_player_rolls[player_id].roll_round + 1))
+        
 
 async def setup(client):
     await client.add_cog(ChanceGames(client))
